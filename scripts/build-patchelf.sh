a9f2921fa7e39483c4e2ff98c68ac508ab538a94
bastimeyer <mail@bastimeyer.de>
2022-11-27 13:57:10 +0100

    build: downgrade patchelf to 0.16.1
    And fix Python segmentation fault after modifying RPATHs

---
+++ scripts/build-patchelf.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+source "$(dirname -- "${BASH_SOURCE[0]}")/_utils.sh"
+
+
+PATCHELF_URL=https://github.com/NixOS/patchelf/releases/download/0.16.1/patchelf-0.16.1.tar.bz2
+PATCHELF_SHA256=ab915f3f4ccc463d96ce1e72685b163110f945c22aee5bc62118d57adff0ab7d
+
+
+build_patchelf() {
+  download_and_extract_tarball "${PATCHELF_URL}" "${PATCHELF_SHA256}" -j --strip-components=1
+
+  ./configure \
+    --prefix=/usr/local
+  make
+  make install
+}
+
+finalize() {
+  rm -rf /usr/local/share/doc/patchelf
+  rm -f /usr/local/share/man/man1/patchelf.1
+}
+
+check() {
+  /usr/local/bin/patchelf --version
+}
+
+
+build build_patchelf
+finalize
+check
